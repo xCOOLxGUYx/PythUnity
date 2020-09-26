6796f9f8826f1901403edb3ec2ddc973f71ec30a
xCOOLxGUYx <68183828+xCOOLxGUYx@users.noreply.github.com>
2020-09-25 21:07:11 -0400

    Add files via upload

---
+++ PythUnity/Variables.py
@@ -0,0 +1,11 @@
+global parts
+parts = []
+
+global deltaTime
+deltaTime = .1
+
+global prefabs
+prefabs = {}
+
+global starts
+starts = []
---
+++ PythUnity/__init__.py
@@ -0,0 +1,2 @@
+from PythUnity.Classes import Object
+from PythUnity.Classes import String
