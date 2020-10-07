956f13f4d71ff84eeedf0cc2c8232e7aba7fa8da
xCOOLxGUYx <68183828+xCOOLxGUYx@users.noreply.github.com>
2020-10-07 08:53:53 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setup(
   download_url = 'https://github.com/xCOOLxGUYx/PythUnity/archive/v_07_1.tar.gz',    # I explain this later on
   keywords = ["Unity", "Python", "Pygame"],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          "pygame"
+          "pygame",
+          "numba"
       ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
